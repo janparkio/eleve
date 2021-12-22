963c43b4a7cc4cfb50d9bc08d27d7595f83614ea
Jan Park <janpark@hotmail.com>
2021-12-22 02:58:12 -0300

    added link to wizard

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-/* This example requires Tailwind CSS v2.0+ */
 export default function Logos() {
   return (
-    <div className=''>
-      <div className='max-w-7xl mx-auto px-4 py-24 sm:px-6 lg:px-8'>
-        <div className='gap-8 grid grid-cols-2 md:grid-cols-4'>
+    <>
+      <div className='max-w-6xl mx-auto px-4 py-24 sm:px-6 lg:px-8'>
+        <div className='grid gap-y-8 gap-x-4 grid-cols-2 md:grid-cols-4'>
           <a
             href='https://www.americana.edu.py/'
             target='_blank'
@@ -13,7 +12,7 @@ export default function Logos() {
           >
             <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
               <img
-                className='h-12'
+                className='h-12 object-contain'
                 src='/images/americana-logo.png'
                 alt='Universidad Americana'
               />
@@ -27,28 +26,35 @@ export default function Logos() {
           >
             <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
               <img
-                className='h-12'
+                className='h-12 object-contain'
                 src='/images/usud-logo.png'
                 alt='Universidad Sudamericana'
               />
             </div>
           </a>
+          <a
+            href='https://www.wizard.com.br/'
+            target='_blank'
+            className='ease-in-out transition-all hover:brightness-110 hover:saturate-0'
+            rel="noreferrer"
+          >
+            <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
+              <img
+                className='h-12 object-contain'
+                src='/images/wizard-logo.png'
+                alt='Wizard Logo'
+              />
+            </div>
+          </a>
           <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
             <img
-              className='h-12'
-              src='/images/wizard-logo.png'
-              alt='Wizard Logo'
-            />
-          </div>
-          <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
-            <img
-              className='h-12'
+              className='h-12 object-contain'
               src='/images/haizea-logo.png'
-              alt='Hazea Logo'
+              alt='Haizea Logo'
             />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
